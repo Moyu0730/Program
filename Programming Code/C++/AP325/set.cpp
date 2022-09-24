b45943e0f2323065945f43ff5fd7fa881640a717
Moyu0730 <aslanchen0730@gmail.com>
2022-09-24 13:09:04 +0800

    2022.09.24 - Setup

--- Programming Code/C++/AP325/set.cpp
+++ Programming Code/C++/AP325/set.cpp
@@ -5,10 +5,11 @@
 #include<stack>
 #include<algorithm>
 using namespace std;
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 const int MAXN = 1e8 + 50;
 const int Mod = 1e9 + 7;
 
 signed main(){
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    opt;
 }
