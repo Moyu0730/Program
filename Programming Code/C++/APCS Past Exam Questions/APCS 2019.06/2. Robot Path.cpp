39fb400d1ccce01928c7d71d52bbc8569d49fabe
Moyu0730 <aslanchen0730@gmail.com>
2022-09-25 09:19:58 +0800

    2022.09.25 - Robot Path Finish

--- Programming Code/C++/APCS Past Exam Questions/APCS 2019.06/2. Robot Path.cpp
+++ Programming Code/C++/APCS Past Exam Questions/APCS 2019.06/2. Robot Path.cpp
@@ -6,6 +6,7 @@
 #include<algorithm>
 using namespace std;
 #define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
 
 const int MAXN = 1e3 + 50;
 const int Mod = 1e9 + 7;
@@ -15,7 +16,7 @@ signed main(){
     opt;
     int n, m;
     cin >> n >> m;
-    int arr[MAXN][MAXN], s = MAXN, x, y;
+    int arr[MAXN][MAXN], s = 1e10, x, y; // Remember to 
     bool visited[MAXN][MAXN] = {false};
     for( int i = 0 ; i < n ; i++ ){
         for( int j = 0 ; j < m ; j++ ){
@@ -28,14 +29,14 @@ signed main(){
         } 
     }
     
-    int ans = 0, cmp, a, b;
+    int cmp, a, b, ans = 0;
     bool flag = true;
     pair<int, int> ind;
     while( flag == true ){
         flag = false;
         ans += arr[y][x];
         visited[y][x] = true;
-        cmp = MAXN;
+        cmp = 1e10;
         for( int t = 0 ; t < 4 ; t++ ){
             a = y + dir[t].first;
             b = x + dir[t].second;
